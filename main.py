01d08af44d61bd6dd9a2af61709538b4ebbd5b16
Rostislav <zagigtovr97@gmail.com>
2023-07-15 02:02:54 +0500

    добавление нового пользователя и ещё пару нужных функций

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import datetime
+
 from src.data import db_session
 from src.data.users import User
+from src.data.subscription import Subscription
+from src.data.history import History
+import more_itertools as mit
+from sqlalchemy import and_, all_
+
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -25,6 +32,13 @@ async def cmd_start(message: types.Message):
     """
     приветственное сообщение
     """
+    session = db_session.create_session()
+    res = session.query(User).filter(User.tg_id == message.from_id).first()
+    if not res:
+        user = User(tg_id=message.from_id)
+        session.add(user)
+        session.commit()
+    session.close()
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton(text="Meta", callback_data="meta0"))
     buttons = [
@@ -68,20 +82,28 @@ async def subscription_list(call: types.CallbackQuery):
 async def all_sub(call: types.CallbackQuery):
     """
     функция выода доступных подписок
-    days: если 1, то меньше или равно 30, если 2б то от 30 до 90, если 3 то больше или равно 90
+    days: если 1, то меньше или равно 30, если 2, то от 30 до 90, если 3 то больше или равно 90
     name_servise: название сервиса
     """
     tmp = call.data.split('|')
-    days = tmp[1]
-    name_servise = tmp[2]
+    vir = lambda x: x.duration - (datetime.datetime.now() - x.created_date).days
+    flag = [lambda x: vir(x) < 31, lambda x: 30 < vir(x) < 91, lambda x: vir(x) > 90][int(tmp[1]) - 1]
+    name_servise = 'кинопоиск'  # tmp[2] пока оставь так, в данной версии у тебя пока что стоит только kino
+
+    sess = db_session.create_session()
+    sub = sess.query(Subscription).filter(Subscription.name == name_servise).all()  # фильтрация только по имени
+    print(sub[0].duration, sub[0].name, sub[0].created_date)
+    sub = list(filter(flag, sub))  # фильтрация по оставшемуся времени(я заколебался в потоке пытаться сделать)
+    print('aaa', sub)
+    but = []
+    for i in sub:
+        ostat = (datetime.datetime.now().date() - i.created_date.date()).days
+        but.append(types.InlineKeyboardButton(text=f"{ostat} дней {ostat * i.price}р",
+                                                  callback_data=f"payment|{ostat}|{ostat * i.price}"))
+    but = list(mit.chunked(but, 3))
+    print(but)
     buttons = [
-        [types.InlineKeyboardButton(text="29 дней 290р",
-                                    callback_data=f"payment|29|290"),
-         types.InlineKeyboardButton(text="10 дней 100р",
-                                    callback_data=f"payment10|100"),
-         types.InlineKeyboardButton(text="17 дней 170р",
-                                    callback_data=f"payment|17|170")
-         ],
+        *but,  # ЧТО ТУТ НЕ ТАК? Я УСТАЛ...(ПЕРЕПРОБОВАЛ ВСЕ КОМБИНАЦИИ)
         [types.InlineKeyboardButton(text="назад", callback_data="subscription_list")
          ]
     ]
@@ -123,10 +145,14 @@ async def successful_payment(message: types.Message):
 
 if __name__ == "__main__":
     # Запуск бота
-    executor.start_polling(dp, skip_updates=True)
     db_session.global_init('src/db/TestDB.sqlite')
     session = db_session.create_session()
-    user = User(tg_id=123123)
-    session.add(user)
-    session.commit()
+    # for i in [['окко', 13.33, 30, datetime.date(2023, 7, 1)], ['премьер', 6.66, 32, datetime.date(2023, 7, 13)],
+    #          ['кинопоиск', 6.66, 65, datetime.date(2023, 7, 13)], ['иви', 13.33, 92, datetime.date(2023, 6, 13)]]:
+    #     n, p, d, c = i
+    #     session.add(Subscription(name=n, price=p, duration=d, created_date=c))
+    # session.commit()
     session.close()
+    executor.start_polling(dp, skip_updates=True)
+
+
--- src/data/subscription.py
+++ src/data/subscription.py
@@ -1,16 +1,19 @@
 import datetime
 
+from sqlalchemy.orm import Mapped
+
 from src.data.db_session import SqlAlchemyBase
-from sqlalchemy import Column, Integer, DateTime, String
+from sqlalchemy import Column, Integer, DateTime, String, Float
 
 
 class Subscription(SqlAlchemyBase):
     __tablename__ = 'Subscriptions'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    created_date = Column(DateTime, default=datetime.datetime.now)
+    name = Column(String, nullable=False)
+    created_date: Mapped[datetime.date] = Column(DateTime, default=datetime.datetime.now)
     duration = Column(Integer, nullable=False)
-    price = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
     users_id = Column(String)
 
 
